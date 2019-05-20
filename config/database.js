b9fcfe32cc7bea4c1baa1894244ae1d8818ea9c9
hobs <hobianarazakanaivo@gmail.com>
2019-05-20 23:01:00 +0300

    database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
   module.exports = {
       //database: "mongodb://localhost:27017/myproject",
-      //database: "mongodb+srv://mean:mean@cluster0-wjvqt.mongodb.net/test?retryWrites=true",
+      database: "mongodb://nantenaina:nantenaina@cluster0-shard-00-00-wjvqt.mongodb.net:27017,cluster0-shard-00-01-wjvqt.mongodb.net:27017,cluster0-shard-00-02-wjvqt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
       //secret:"mean"
-      database: "mongodb://diary:diary@ds151908.mlab.com:51908/youplay",
-      secret:"diary"
+      //database: "mongodb://diary:diary@ds151908.mlab.com:51908/youplay",
+      secret:"nantenaina"
   }
